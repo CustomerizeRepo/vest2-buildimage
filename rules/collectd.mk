1c33be718eb7f1083ddce5dc5a5f18fd7d072406
squid_ro <squid_ro@edge-core.com>
2018-09-12 15:17:42 +0800

    Install collectd packages to rootfs.

---
+++ rules/collectd.mk
@@ -0,0 +1,10 @@
+#     SOME_NEW_DEB = some_new_deb.deb
+#     $(SOME_NEW_DEB)_URL = https://url/to/this/deb.deb
+#     SONIC_ONLINE_DEBS += $(SOME_NEW_DEB)
+
+COLLECTD_DEB = collectd_5.7.0-3~bpo7+2_amd64.deb
+COLLECTD_CORE_DEB = collectd-core_5.7.0-3~bpo7+2_amd64.deb
+
+$(COLLECTD_DEB)_URL = http://ftp.tw.debian.org/debian/pool/main/c/collectd/$(COLLECTD_DEB)
+$(COLLECTD_CORE_DEB)_URL = http://ftp.tw.debian.org/debian/pool/main/c/collectd/$(COLLECTD_CORE_DEB)
+SONIC_ONLINE_DEBS += $(COLLECTD_DEB) $(COLLECTD_CORE_DEB) 
